7a8715f0b9b1b3570c85fd4b4b1e52fc28e4cfb3
joan-ouma <joanouma48@gmail.com>
2025-12-02 22:17:59 +0300

    add geometry.js file for shape definitions

---
+++ geometry.js
@@ -0,0 +1,80 @@
+// geometry.js - Shape Definitions
+
+// Buffers
+var cubeBuffer;
+var cubeIndexBuffer;
+var terrainBuffer;
+var terrainIndexBuffer;
+
+function initBuffers() {
+    // 1. CUBE (Jeep Body & Wheels)
+    cubeBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeBuffer);
+    var vertices = [
+        -1, -1, 1,  1, -1, 1,  1, 1, 1,  -1, 1, 1, // Front
+        -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1, // Back
+        -1, 1, -1, -1, 1, 1,  1, 1, 1,  1, 1, -1, // Top
+        -1, -1, -1, 1, -1, -1, 1, -1, 1, -1, -1, 1, // Bottom
+        1, -1, -1, 1, 1, -1, 1, 1, 1,  1, -1, 1, // Right
+        -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1 // Left
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    cubeBuffer.itemSize = 3; cubeBuffer.numItems = 24;
+
+    cubeIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
+    var indices = [0,1,2, 0,2,3, 4,5,6, 4,6,7, 8,9,10, 8,10,11, 12,13,14, 12,14,15, 16,17,18, 16,18,19, 20,21,22, 20,22,23];
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+    cubeBuffer.numItems = 36;
+
+    // 2. TERRAIN (The Hills)
+    // We generate a grid of points
+    terrainBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, terrainBuffer);
+
+    var terrainVertices = [];
+    var size = 60; // Size of the map
+    var step = 1;  // Resolution
+
+    for (var x = -size; x <= size; x += step) {
+        for (var z = -size; z <= size; z += step) {
+            // Requirement: Heightmap function (Sine patterns)
+            var y = getTerrainHeight(x, z);
+            terrainVertices.push(x, y - 2.5, z); // Shift down slightly
+        }
+    }
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(terrainVertices), gl.STATIC_DRAW);
+    terrainBuffer.itemSize = 3;
+
+    terrainIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, terrainIndexBuffer);
+
+    var terrainIndices = [];
+    var rows = (size * 2) / step + 1;
+    var cols = (size * 2) / step + 1;
+
+    for (var i = 0; i < rows - 1; i++) {
+        for (var j = 0; j < cols - 1; j++) {
+            var topLeft = i * cols + j;
+            var topRight = topLeft + 1;
+            var bottomLeft = (i + 1) * cols + j;
+            var bottomRight = bottomLeft + 1;
+
+            terrainIndices.push(topLeft, bottomLeft, topRight);
+            terrainIndices.push(topRight, bottomLeft, bottomRight);
+        }
+    }
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(terrainIndices), gl.STATIC_DRAW);
+    terrainBuffer.numItems = terrainIndices.length;
+}
+
+// Helper to calculate height at any X,Z
+function getTerrainHeight(x, z) {
+    var y = Math.sin(x * 0.1) * 2.0 + Math.cos(z * 0.1) * 2.0;
+    // Flat area near start
+    if(Math.abs(x) < 5 && Math.abs(z) < 15) {
+        y *= 0.2;
+    }
+    return y;
+}
